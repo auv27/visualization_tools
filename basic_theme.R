e5e9d90096211c9450bd64b52af704496bd5c009
Alicia Vallorani <auv27@psu.edu>
2023-09-21 11:49:08 -0400

    update

--- basic_theme.R
+++ basic_theme.R
@@ -1,11 +1,6 @@
 library(tidyverse)
 
-# This saves basic settings to standardize across figures for a manuscript
-# Use source() to read this theme in at the beginning of a script
-# Then theme_set() to make these settings standard for all figures in script
-# Reduces the amount of times code needs to be repeated to create figures
-
-basic_theme <- theme_bw() +
+theme_bw() +
 theme(panel.grid.major.x = element_blank(),
       panel.grid.minor.x = element_blank(),
       panel.grid.major.y = element_blank(),
@@ -14,4 +9,4 @@ theme(panel.grid.major.x = element_blank(),
 theme(axis.text = element_text(size = 12, color = "black")) +
 theme(axis.title = element_text(size = 14, color = "black")) +
 theme(legend.text = element_text(size = 12, color = "black")) +
-theme(legend.title = element_text(size = 14, color = "black"))
+theme(legend.title = element_text(size = 14, color = "black")) -> basic_theme
